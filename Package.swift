01a96f5c64b635d669dfabcedb827b0f4fbb36d9
Paul Schifferer <paul@schifferers.net>
2021-06-20 20:22:36 -0700

    Checkpoint.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,36 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.4
 import PackageDescription
 
 
 let package = Package(
-    name: "GameSystemObjects",
-    products: [
-        .library(name: "GameSystemObjects", targets: ["GameSystemObjects"]),
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-    ],
-    targets: [
-        .target(name: "GameSystemObjects", dependencies: ["Vapor"]),
-        .testTarget(name: "GameSystemObjectsTests", dependencies: ["GameSystemObjects"]),
-    ]
+        name: "sweetrpg-gamesystem-model",
+        platforms: [
+            .macOS(.v10_15),
+        ],
+        products: [
+            .library(name: "GameSystemModel", targets: [ "GameSystemModel" ])
+        ],
+        dependencies: [
+            .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+            .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        ],
+        targets: [
+            .target(
+                    name: "GameSystemModel",
+                    dependencies: [
+                        .product(name: "Fluent", package: "fluent"),
+                        .product(name: "Vapor", package: "vapor"),
+                    ],
+                    swiftSettings: [
+                        // Enable better optimizations when building in Release configuration. Despite the use of
+                        // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                        // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
+                        .unsafeFlags([ "-cross-module-optimization" ], .when(configuration: .release)),
+                    ]
+            ),
+            .testTarget(name: "GameSystemModelTests", dependencies: [
+                .target(name: "GameSystemModel"),
+                .product(name: "XCTVapor", package: "vapor"),
+            ]),
+        ]
 )
